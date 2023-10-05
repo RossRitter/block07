29356298d95b4330fcf333460555162fb5787e9e
Ross Ritter <rossritter@Rosss-MBP.attlocal.net>
2023-10-05 13:59:04 -0400

    vault numbers

---
+++ index.js
@@ -0,0 +1,11 @@
+// Creating a Vault with the combination 10 - 40 - 39
+//must use three different arithmetic operators that will generate each individual number
+//display the combination on the HTML page, or in an alert popup
+let openVaultMessage = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+const firstNumber = (5 * 2); //using const becasue I want the number to be the same always 
+console.log (firstNumber); // outut should be 10
+const secondNumber = (20 + 20); 
+console.log (secondNumber); // output should be 40
+const thirdNumber = (50 - 11);
+console.log (thirdNumber); // output should be 39
+
